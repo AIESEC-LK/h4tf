5dcc850d30a1fb0655d038f409224313e01e2900
kaneeldias <kaneeldias@hotmail.com>
2021-08-29 03:22:44 +0530

    Add profile information

--- src/app/interfaces/participant.ts
+++ src/app/interfaces/participant.ts
@@ -2,5 +2,12 @@ export interface Participant {
   first_name: string,
   last_name: string,
   email: string,
-  status: string
+  status: string,
+  phone: string,
+  university: string,
+  country: string,
+  interest: string,
+  entity: string,
+  year: string,
+  cv_filename: string
 }
